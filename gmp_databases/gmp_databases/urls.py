0374eec7defe8a67b40aabcc4e093c919b36c52f
Ori Kalinski <ori@mintigo.com>
2017-01-17 07:08:38 +0200

    bla'

--- gmp_databases/gmp_databases/urls.py
+++ gmp_databases/gmp_databases/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from views import home_page_stats, filter_places_by_opening_hours_and_type, \
     filter_places_by_address_and_rating, filter_by_all_params, get_place_details, \
-    get_place_images, insert_image
+    get_place_images, insert_image, insert_review
 
 
 from django.contrib import admin
@@ -19,5 +19,6 @@ urlpatterns = patterns('',
     url('^full_search/', filter_by_all_params),
     url('^place/', get_place_details),
     url('^gallery/', get_place_images),
-    url('^uploads/', insert_image)
+    url('^uploads/', insert_image),
+    url('^insert_review/', insert_review)
 )
--- gmp_databases/gmp_databases/views.py
+++ gmp_databases/gmp_databases/views.py
@@ -1,11 +1,22 @@
 from time import time
 import os
 from django.shortcuts import render
-from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
 
 from models import Place, Review, Image, City
 from django.db.models import Q, Avg, Count
+import mysql.connector
+
+host = "mysqlsrv.cs.tau.ac.il"
+user = "DbMysql13"
+password = "DbMysql13"
+dbname = "DbMysql13"
+try:
+    conn = mysql.connector.connect(host=host, database=dbname, user=user, password=password)
+    if conn.is_connected():
+        print('Connected to MySQL database')
+    cur = conn.cursor()
+except mysql.connector.Error:
+    pass
 
 
 FIELDS = ["website", "rating", "name", "location__formatted_address", "id"]
@@ -36,9 +47,10 @@ def handle_times(start_time, end_time):
     return start_time, end_time
 
 
-def get_results(places):
-    results = [dict({"numeric_id": numeric_id + 1}, **place_dict) for numeric_id, place_dict
-               in enumerate(places.values(*FIELDS))]
+def get_results(cur):
+    fields = [row[0] for row in cur.description]
+    results = [dict({"numeric_id": numeric_id + 1}, **dict(zip(fields, place_tuple))) for numeric_id, place_tuple
+               in enumerate(cur.fetchall())]
     return results
 
 
@@ -63,14 +75,38 @@ def filter_places_by_opening_hours_and_type(request):
     start_time = request.GET.get("open_time")
     end_time = request.GET.get("close_time")
     place_type = request.GET.get("type")
-    order_by = None
-    limit = None
+    # order_by = None
+    # limit = None
     if day:
         start_time, end_time = handle_times(start_time, end_time)
-        places = get_filter_by_opening_hours(Place.objects, day, start_time, end_time)
-        places = places.filter(types__name=place_type)
-        places = get_ordered_limited_places(places, order_by, limit)
-        results = get_results(places)
+        query = """
+        SELECT DISTINCT
+        gmp_databases_place.website,
+        gmp_databases_place.rating,
+        gmp_databases_place.name,
+        gmp_databases_location.formatted_address,
+        gmp_databases_place.id
+        FROM
+            gmp_databases_place
+                INNER JOIN
+            gmp_databases_place_opening_hours ON (gmp_databases_place.id = gmp_databases_place_opening_hours.place_id)
+                INNER JOIN
+            gmp_databases_openinghours ON (gmp_databases_place_opening_hours.openinghours_id = gmp_databases_openinghours.id)
+                INNER JOIN
+            gmp_databases_place_types ON (gmp_databases_place.id = gmp_databases_place_types.place_id)
+                INNER JOIN
+            gmp_databases_type ON (gmp_databases_place_types.type_id = gmp_databases_type.id)
+                LEFT OUTER JOIN
+            gmp_databases_location ON (gmp_databases_place.location_id = gmp_databases_location.id)
+        WHERE
+            (gmp_databases_openinghours.day = %s
+                AND (gmp_databases_openinghours.open BETWEEN %s AND %s
+                OR gmp_databases_openinghours.close BETWEEN %s AND %s)
+                AND gmp_databases_type.name = %s)
+        ORDER BY gmp_databases_place.rating DESC
+        """
+        cur.execute(query, (day, start_time, end_time, start_time, end_time, place_type))
+        results = get_results(cur)
         searched_for_results = True
     else:
         results = list()
@@ -82,7 +118,7 @@ def filter_places_by_opening_hours_and_type(request):
 def filter_places_by_address_and_rating(request, address, radius, bottom_rating, order_by, limit):
     places = Place.objects.filter(rating__gt=bottom_rating)
     places = get_ordered_limited_places(places, order_by, limit)
-    return render(request, 'first_html.html', {'results': list(places.values())})
+    return render(request, 'ratingAddrRadius.html', {'results': list(places.values())})
 
 
 def filter_by_all_params(request, day, starttime, endtime,
@@ -91,7 +127,7 @@ def filter_by_all_params(request, day, starttime, endtime,
     places = get_filter_by_opening_hours(places, day, starttime, endtime)
     places = places.filter(types__name=type_name)
     places = get_ordered_limited_places(places, order_by, limit)
-    return render(request, 'first_html.html', {'results': list(places.values())})
+    return render(request, 'detailsTable.html', {'results': list(places.values())})
 
 
 def insert_image(request):
@@ -104,12 +140,12 @@ def insert_image(request):
 
 
 def insert_review(request):
-    author_name = request.POST.get("author_name")
-    place_id = request.POST.get("place_id")
-    rating = request.POST.get("rating")
-    text = request.POST.get("text")
+    author_name = request.GET.get("author_name")
+    place_id = request.GET.get("place_id")
+    rating = request.GET.get("rating")
+    text = request.GET.get("text")
     Review.objects.create(author_name=author_name, place_id=place_id, rating=rating, text=text)
-    return HttpResponseRedirect(reverse("first_html.html"))
+    get_place_details(request)
 
 
 def home_page_stats(request):
@@ -135,14 +171,50 @@ def get_complicated_stats(request):
 def get_place_details(request):
     place_id = request.GET.get("place_id")
     place = Place.objects.filter(id=place_id)
-    place_details = place.values().first()
-    place = place.first()
+    cur.execute("""SELECT
+                    *
+                   FROM
+                       gmp_databases_place
+                   WHERE
+                       gmp_databases_place.id = %s
+                   LIMIT 1""", (place_id, ))
+    place_details = get_results(cur)[0]
     place_details["place_id"] = place_details["id"]
-    place_details["reviews"] = place.review_set.values()
-    place_details["image_url"] = place.image_set.values_list("url", flat=True).first() \
+    cur.execute("""
+                SELECT
+                *
+                FROM
+                    gmp_databases_review
+                WHERE
+                    gmp_databases_review.place_id = %s""", (place_id, ))
+    place_details["reviews"] = get_results(cur)
+    cur.execute("""SELECT
+                    gmp_databases_image.url
+                FROM
+                    gmp_databases_image
+                WHERE
+                    gmp_databases_image.place_id = %s
+                LIMIT 1""", (place_id, ))
+    place_details["image_url"] = get_results(cur)[0]["url"] \
         or "https://www.raise.sg/membership/web/images/noimage.jpg"
-    place_details["types"] = place.types.values()
-    place_details["opening_hours_list"] = place.opening_hours.values()
+    cur.execute("""SELECT
+                    *
+                FROM
+                    gmp_databases_type
+                        INNER JOIN
+                    gmp_databases_place_types ON (gmp_databases_type.id = gmp_databases_place_types.type_id)
+                WHERE
+                    gmp_databases_place_types.place_id = %s""", (place_id, ))
+    place_details["types"] = get_results(cur)
+    cur.execute("""SELECT
+                    *
+                FROM
+                    gmp_databases_openinghours
+                        INNER JOIN
+                    gmp_databases_place_opening_hours ON (gmp_databases_openinghours.id = gmp_databases_place_opening_hours.openinghours_id)
+                WHERE
+                    gmp_databases_place_opening_hours.place_id = %s""", (place_id, ))
+    place_details["opening_hours_list"] = get_results(cur)
     return render(request, "place.html", place_details)
 
 
